a4e0628906d8d0faf200715f9361a88bca320da3
StephhyL <Stephanie Li>
2021-11-04 14:42:55 -0400

    added index, working half way

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,12 +1,27 @@
 const request = require('request');
 // const fs = require('fs');
 
-// grab the value from the command line
-const input = process.argv.slice(2);
-const inputString = input[0];
-const query = inputString.slice(0, 4);
 
-const domain = `https://api.thecatapi.com/v1/breeds/search?q=${query}`;
+
+const fetchBreedDescription = (breedName, callback) => {
+  const domain = `https://api.thecatapi.com/v1/breeds/search?q=${query}`;
+
+
+
+
+
+
+
+
+
+
+
+
+  callback(error, value);
+};
+
+
+
 
 
 request(domain, (error, response, body) => {
@@ -20,19 +35,7 @@ request(domain, (error, response, body) => {
     return;
   }
 
-  const data = JSON.parse(body); //data is now body but as an object
-
-
-  // console.log("body is: ", body, "and it's type is:", typeof body);
-  // console.log("data is: ", data, "and its type is: ", typeof data)
-
-
-
-
-  // console.log(data);
-  // console.log(typeof data);
-  // console.log(data);
-  // console.log(data == true); ???? why --> an empty arr is still something
+  const data = JSON.parse(body); //data is now body but as an 
   if (data[0]) {
     const catDescription = data[0].description;
     console.log(catDescription);
@@ -43,3 +46,4 @@ request(domain, (error, response, body) => {
   // console.log(typeof body); // body is a string
 });
 
+module.exports = fetchBreedDescription;
