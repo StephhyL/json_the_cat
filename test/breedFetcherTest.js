f96ffa2d69d93ae0a603561930658c7eb6949480
StephhyL <Stephanie Li>
2021-11-04 15:29:46 -0400

    linted

--- test/breedFetcherTest.js
+++ test/breedFetcherTest.js
@@ -8,16 +8,16 @@ describe('fetchBreedDescription', () => {
       const expectedDesc = "The Siberians dog like temperament and affection makes the ideal lap cat and will live quite happily indoors. Very agile and powerful, the Siberian cat can easily leap and reach high places, including the tops of refrigerators and even doors.";
       assert.equal(expectedDesc, desc.trim());
       done();
-    })
-  })
+    });
+  });
 
   it('should return an error object description for an invalid breed, via callback', (done) => {
     fetchBreedDescription('GUUUUUUTA', (err, desc) => {
-      const expectedError = "Breed not found"
+      const expectedError = "Breed not found";
       assert.equal(err, expectedError);
       assert.equal(desc, null);
       done();
-    })
-  })
+    });
+  });
 
-})
+});
